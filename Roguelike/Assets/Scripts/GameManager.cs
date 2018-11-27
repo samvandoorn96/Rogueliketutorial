53ede55d3982cb84cb6c8689377a0a1bb6d138b3
Sam van Doorn <samvandoorn96@gmail.com>
2018-11-27 17:15:54 +0100

    final

--- Roguelike/Assets/Scripts/GameManager.cs
+++ Roguelike/Assets/Scripts/GameManager.cs
@@ -1,16 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;             
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    public float turnDelay = .1f;
+    public float levelStartDelay = 2f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Text levelText;
+    private GameObject levelImage;
+    private bool doingSetup;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private int level = 1;                                  
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        enemies = new List<Enemy>();
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    private void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+    void InitGame()
+    {
+        doingSetup = true;
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        enemies.Clear();
+        boardScript.SetupScene(level);
+        Invoke("HideLevelImage", levelStartDelay);
+    }
+
+    public void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+
+    private void Update()
+    {
+        if (playersTurn || enemiesMoving || doingSetup)
+        {
+            return;
+        }
+        StartCoroutine(MoveEnemies());
+    }
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
 }
